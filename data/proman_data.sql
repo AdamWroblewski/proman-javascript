1a7c52bdd0ae50562fd0d3441f7b05fe25b2ada0
Adam <adamwroblewski.1993@gmail.com>
2019-11-26 14:39:36 +0100

    Change owner

--- data/proman_data.sql
+++ data/proman_data.sql
@@ -17,14 +17,14 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Data for Name: proman_users; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: proman_users; Type: TABLE DATA; Schema: public; Owner: adam
 --
 
 INSERT INTO public.proman_users (id, name, pwd) VALUES (1, 'admin', '');
 
 
 --
--- Data for Name: proman_boards; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: proman_boards; Type: TABLE DATA; Schema: public; Owner: adam
 --
 
 INSERT INTO public.proman_boards (id, title, private, user_id) VALUES (1, 'Board 1', false, 1);
@@ -32,7 +32,7 @@ INSERT INTO public.proman_boards (id, title, private, user_id) VALUES (2, 'Board
 
 
 --
--- Data for Name: proman_cards; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: proman_cards; Type: TABLE DATA; Schema: public; Owner: adam
 --
 
 INSERT INTO public.proman_cards (id, board_id, title, status_id, orderr) VALUES (1, 1, 'new card 1', 0, 0);
@@ -50,7 +50,7 @@ INSERT INTO public.proman_cards (id, board_id, title, status_id, orderr) VALUES 
 
 
 --
--- Data for Name: proman_statuses; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: proman_statuses; Type: TABLE DATA; Schema: public; Owner: adam
 --
 
 INSERT INTO public.proman_statuses (id, title) VALUES (0, 'new');
@@ -60,28 +60,28 @@ INSERT INTO public.proman_statuses (id, title) VALUES (3, 'done');
 
 
 --
--- Name: proman_boards_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: proman_boards_id_seq; Type: SEQUENCE SET; Schema: public; Owner: adam
 --
 
 SELECT pg_catalog.setval('public.proman_boards_id_seq', 1, false);
 
 
 --
--- Name: proman_cards_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: proman_cards_id_seq; Type: SEQUENCE SET; Schema: public; Owner: adam
 --
 
 SELECT pg_catalog.setval('public.proman_cards_id_seq', 1, false);
 
 
 --
--- Name: proman_statuses_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: proman_statuses_id_seq; Type: SEQUENCE SET; Schema: public; Owner: adam
 --
 
 SELECT pg_catalog.setval('public.proman_statuses_id_seq', 1, false);
 
 
 --
--- Name: proman_users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: proman_users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: adam
 --
 
 SELECT pg_catalog.setval('public.proman_users_id_seq', 1, false);
